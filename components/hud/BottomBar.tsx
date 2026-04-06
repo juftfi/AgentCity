faac7d72114b4db819a37a677a491712c0aa1321
juftfi <fourswapxyz@gmail.com>
2026-04-06 20:05:56 +0700

    Update BottomBar.tsx

--- components/hud/BottomBar.tsx
+++ components/hud/BottomBar.tsx
@@ -55,7 +55,7 @@ export default function BottomBar({ connection, sessionMetrics, seats }: BottomB
       <div className="hud-pill hud-pill--metric">
         <Sparkles size={10} />
         <span>
-          CA: AG2ai1nt9AHjEUwMpXMDkEq5d4FagquLhNgLrF6spump
+          CA: 4444
         </span>
       </div>
       <div className="hud-pill hud-pill--metric">
